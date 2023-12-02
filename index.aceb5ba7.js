aed3d8e1ec7bc95689e3c0a0ded78ecf3b8ea264
newqou <137334905+newqou@users.noreply.github.com>
2023-12-02 18:00:43 +0200

    make build

---
+++ index.aceb5ba7.js
@@ -0,0 +1,2 @@
+!function e(t,n){let r=document.createElement("ul");for(let l in t.append(r),n){let t=document.createElement("li");t.textContent=l,r.append(t),Object.keys(n[l]).length>0&&e(t,n[l])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.aceb5ba7.js.map
